0ef95406db59139cbe954ad15a58a01bae52f235
Russell Mirabelli <russell@mirabellifamily.org>
2023-10-03 09:17:13 -0500

    tuesday morning, again

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/transaction.dart';
-import 'package:intl/intl.dart';
-import 'widgets/transaction_list.dart';
+import 'widgets/user_transaction.dart';
 
 void main() {
   runApp(MainApp());
@@ -9,9 +7,6 @@ void main() {
 
 class MainApp extends StatelessWidget {
   MainApp({super.key});
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,36 +25,7 @@ class MainApp extends StatelessWidget {
                 child: Text('Chart'),
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      controller: titleController,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(labelText: 'title'),
-                    ),
-                    TextField(
-                      controller: amountController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(labelText: 'amount'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                      child: Text('add transaction'),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            TransactionList(),
+            UserTransaction(),
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -2,84 +2,67 @@ import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  State<TransactionList> createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  static List<Transaction> transactions = [
-    Transaction(
-      id: UniqueKey().toString(),
-      title: 'new shoes',
-      amountInPennies: 3299,
-    ),
-    Transaction(
-      id: UniqueKey().toString(),
-      title: 'groceries',
-      amountInPennies: 12332,
-    ),
-    Transaction(
-      id: UniqueKey().toString(),
-      title: 'coffee',
-      amountInPennies: 4275,
-    ),
-  ];
+class TransactionList extends StatelessWidget {
+  TransactionList(this.transactions);
+  final List<Transaction> transactions;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: transactions.map((transaction) {
-          return Card(
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            Transaction transaction = transactions[index];
+            return Card(
               child: Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.black,
-                    )),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                child: Text(
-                  '\$${transaction.amountInPennies / 100.0}'.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    transaction.title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.blueGrey,
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.black,
+                        )),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    child: Text(
+                      '\$${(transaction.amountInPennies / 100.0).toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat.MEd().format(transaction.date),
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.grey,
-                    ),
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.MEd().format(transaction.date),
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 13,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ));
-        }).toList(),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
